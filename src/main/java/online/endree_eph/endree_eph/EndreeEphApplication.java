b1f4d129654ce13cc32ece478865a33179912c6e
Endree Eph <fiadotsyeu@gmail.com>
2024-03-27 08:55:53 +0100

    created a project and added security configurations

---
+++ src/main/java/online/endree_eph/endree_eph/EndreeEphApplication.java
@@ -0,0 +1,12 @@
+package online.endree_eph.endree_eph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndreeEphApplication {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(EndreeEphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/endree_eph/endree_eph/EndreeEphApplicationTests.java
@@ -0,0 +1,13 @@
+package online.endree_eph.endree_eph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndreeEphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
